1b6ac39d3bb2842bd15b3382882f36b4c9bf7941
GoldenFox013 <borok.maja@gmail.com>
2025-12-02 13:26:51 +0100

    orai munka

---
+++ 12.02/json-api-demo/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import(&#39;tailwindcss&#39;).Config} */
+module.exports = {
+content: [
+"./index.html",
+"./src/**/*.{vue,js,ts,jsx,tsx}",
+],
+theme: {
+extend: {},
+},
+plugins: [],
+}
